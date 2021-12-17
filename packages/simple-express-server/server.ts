e2ad6effb15541c76f39884e5231464ff383853b
Alex Eagleson <alexeagleson@gmail.com>
2021-12-17 01:16:25 -0500

    first commit

---
+++ packages/simple-express-server/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import _ from "lodash";
+const app = express();
+const port = 3001;
+
+export interface QueryPayload {
+  payload: string;
+}
+
+app.use((_req, res, next) => {
+  // Allow any website to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Continue to next middleware
+  next();
+});
+
+app.get("/", (_req, res) => {
+  const responseData: QueryPayload = {
+    payload: _.snakeCase("Server data returned successfully"),
+  };
+
+  res.json(responseData);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ packages/simple-react-app/src/App.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import _ from "lodash";
+import { QueryPayload } from "simple-express-server/server";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {_.toUpper("Learn React")}
+        </a>
+        <button
+          onClick={() => {
+            fetch("http://localhost:3001/", {})
+              .then((response) => response.json())
+              .then((data: QueryPayload) => console.log(data.payload));
+          }}
+        >
+          GET SOME DATA
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
